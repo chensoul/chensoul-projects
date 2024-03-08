4d64cd7a117e5e385ddc7a3d6b3165df1f7dfcec
chensoul <ichensoul@gmail.com>
2024-03-08 15:14:30 +0800

    docker部署

--- test-em-all.sh
+++ test-em-all.sh
@@ -10,77 +10,70 @@
 
 function assertCurl() {
 
-  local expectedHttpCode=$1
-  local curlCmd="$2 -w \"%{http_code}\""
-  local result=$(eval $curlCmd)
-  local httpCode="${result:(-3)}"
-  RESPONSE='' && (( ${#result} > 3 )) && RESPONSE="${result%???}"
+    local expectedHttpCode=$1
+    local curlCmd="$2 -w \"%{http_code}\""
+    local result=$(eval $curlCmd)
+    local httpCode="${result:(-3)}"
+    RESPONSE='' && ((${#result} > 3)) && RESPONSE="${result%???}"
 
-  if [ "$httpCode" = "$expectedHttpCode" ]
-  then
-    if [ "$httpCode" = "200" ]
-    then
-      business_code=$(echo $RESPONSE | jq .code)
-      if [ "$business_code" = "0" ] || [ "$business_code" = null ]
-      then
-        echo "Test OK (HTTP Code: $httpCode)"
-      else
-        echo  "Test FAILED, EXPECTED HTTP Code: $expectedHttpCode, GOT: $httpCode -> $business_code, WILL ABORT!"
-        echo  "- Failing command: $curlCmd"
-        echo  "- Response Body: $RESPONSE"
-      fi
+    if [ "$httpCode" = "$expectedHttpCode" ]; then
+        if [ "$httpCode" = "200" ]; then
+            business_code=$(echo $RESPONSE | jq .code)
+            if [ "$business_code" = "0" ] || [ "$business_code" = null ]; then
+                echo "Test OK (HTTP Code: $httpCode)"
+            else
+                echo "Test FAILED, EXPECTED HTTP Code: $expectedHttpCode, GOT: $httpCode -> $business_code, WILL ABORT!"
+                echo "- Failing command: $curlCmd"
+                echo "- Response Body: $RESPONSE"
+            fi
+        else
+            echo "Test OK (HTTP Code: $httpCode, $RESPONSE)"
+        fi
     else
-      echo "Test OK (HTTP Code: $httpCode, $RESPONSE)"
+        echo "Test FAILED, EXPECTED HTTP Code: $expectedHttpCode, GOT: $httpCode, WILL ABORT!"
+        echo "- Failing command: $curlCmd"
+        echo "- Response Body: $RESPONSE"
+        exit 1
     fi
-  else
-    echo  "Test FAILED, EXPECTED HTTP Code: $expectedHttpCode, GOT: $httpCode, WILL ABORT!"
-    echo  "- Failing command: $curlCmd"
-    echo  "- Response Body: $RESPONSE"
-    exit 1
-  fi
 }
 
 function assertEqual() {
 
-  local expected=$1
-  local actual=$2
+    local expected=$1
+    local actual=$2
 
-  if [ "$actual" = "$expected" ]
-  then
-    echo "Test OK (actual value: $actual)"
-  else
-    echo "Test FAILED, EXPECTED VALUE: $expected, ACTUAL VALUE: $actual, WILL ABORT"
-    exit 1
-  fi
+    if [ "$actual" = "$expected" ]; then
+        echo "Test OK (actual value: $actual)"
+    else
+        echo "Test FAILED, EXPECTED VALUE: $expected, ACTUAL VALUE: $actual, WILL ABORT"
+        exit 1
+    fi
 }
 
 function testUrl() {
-  url=$@
-  if $url -ks -f -o /dev/null
-  then
-    return 0
-  else
-    return 1
-  fi;
+    url=${@}
+    if $url -ks -f -o /dev/null; then
+        return 0
+    else
+        return 1
+    fi
 }
 
 function waitForService() {
-  url=$@
-  echo "Wait for: $url... "
-  n=0
-  until testUrl $url
-  do
-    n=$((n + 1))
-    if [[ $n == 30 ]]
-    then
-      echo " Give up"
-      exit 1
-    else
-      sleep 3
-      echo -e ", retry #$n "
-    fi
-  done
-  echo "DONE, continues..."
+    url=${@}
+    echo "Wait for: $url... "
+    n=0
+    until testUrl $url; do
+        n=$((n + 1))
+        if [[ $n == 30 ]]; then
+            echo " Give up"
+            exit 1
+        else
+            sleep 3
+            echo -e ", retry #$n "
+        fi
+    done
+    echo "DONE, continues..."
 }
 
 function testCircuitBreaker() {
@@ -92,11 +85,10 @@ function testCircuitBreaker() {
 
     # Open the circuit breaker by running three slow calls in a row, i.e. that cause a timeout exception
     # Also, verify that we get 500 back and a timeout related error message
-    for ((n=0; n<10; n++))
-    do
+    for ((n = 0; n < 10; n++)); do
         assertCurl 200 "curl http://$HOST:$PORT/auth/user/info?delay=3 $AUTH -s"
         message=$(echo $RESPONSE | jq -r .message)
-#        assertEqual "Did not observe any item or terminal signal within 2000ms" "${message:0:57}"
+        #        assertEqual "Did not observe any item or terminal signal within 2000ms" "${message:0:57}"
     done
 
     # Verify that the circuit breaker is open
@@ -123,8 +115,7 @@ function testCircuitBreaker() {
 
     # Close the circuit breaker by running three normal calls in a row
     # Also, verify that we get 200 back and a response based on information in the product database
-    for ((n=0; n<3; n++))
-    do
+    for ((n = 0; n < 3; n++)); do
         assertCurl 200 "curl http://$HOST:$PORT/auth/user/info $AUTH -s"
         assertEqual "user name C" "$(echo "$RESPONSE" | jq -r .data.username)"
     done
@@ -133,30 +124,28 @@ function testCircuitBreaker() {
     assertEqual "CLOSED" "$(curl -s http://$HOST:$PORT/auth/actuator/health | jq -r .components.circuitBreakers.details.authservice.details.state)"
 
     # Verify that the expected state transitions happened in the circuit breaker
-    assertEqual "CLOSED_TO_OPEN"      "$(curl -s http://$HOST:$PORT/auth/actuator/circuitbreakerevents/authservice/STATE_TRANSITION | jq -r .circuitBreakerEvents[-3].stateTransition)"
-    assertEqual "OPEN_TO_HALF_OPEN"   "$(curl -s http://$HOST:$PORT/auth/actuator/circuitbreakerevents/authservice/STATE_TRANSITION | jq -r .circuitBreakerEvents[-2].stateTransition)"
+    assertEqual "CLOSED_TO_OPEN" "$(curl -s http://$HOST:$PORT/auth/actuator/circuitbreakerevents/authservice/STATE_TRANSITION | jq -r .circuitBreakerEvents[-3].stateTransition)"
+    assertEqual "OPEN_TO_HALF_OPEN" "$(curl -s http://$HOST:$PORT/auth/actuator/circuitbreakerevents/authservice/STATE_TRANSITION | jq -r .circuitBreakerEvents[-2].stateTransition)"
     assertEqual "HALF_OPEN_TO_CLOSED" "$(curl -s http://$HOST:$PORT/auth/actuator/circuitbreakerevents/authservice/STATE_TRANSITION | jq -r .circuitBreakerEvents[-1].stateTransition)"
 }
 
-
 set -e
 
-echo "Start Tests:" `date`
+echo "Start Tests:" "$(date)"
 
 echo "HOST=${HOST}"
 echo "PORT=${PORT}"
 echo "SKIP_CB_TESTS=${SKIP_CB_TESTS}"
 
-if [[ $@ == *"start"* ]]
-then
-  echo "Package and build docker image..."
-  mvn clean package -DskipTest && docker compose build
-  echo "Restarting the test environment..."
-  echo "$ docker compose down --remove-orphans"
-  docker compose down --remove-orphans
-  echo "$ docker compose up -d"
-  docker compose up -d
-  docker ps --format {{.Names}}
+if [[ $* == *"start"* ]]; then
+    echo "Package and build docker image..."
+    mvn clean package -DskipTest && docker compose build
+    echo "Restarting the test environment..."
+    echo "$ docker compose down --remove-orphans"
+    docker compose down --remove-orphans
+    echo "$ docker compose up -d"
+    docker compose up -d
+    docker ps --format "{{.Names}}"
 fi
 
 waitForService curl -k http://$HOST:$PORT/actuator/health
@@ -176,15 +165,14 @@ assertCurl 200 "curl -H "accept:application/json" -k http://root:123456@$HOST:$P
 assertEqual 2 $(echo $RESPONSE | jq ".applications.application | length")
 
 # Verify access to the Config server and that its encrypt/decrypt endpoints work
-assertCurl 200 "curl -H "accept:application/json" -k http://root:123456@$HOST:$PORT/config/foodie-diner/default -s"
+assertCurl 200 "curl -H "accept:application/json" -k http://root:123456@$HOST:$PORT/config/auth-service/default -s"
 TEST_VALUE="hello world"
 ENCRYPTED_VALUE=$(curl -k http://root:123456@$HOST:$PORT/config/encrypt --data-urlencode "$TEST_VALUE" -s)
 DECRYPTED_VALUE=$(curl -k http://root:123456@$HOST:$PORT/config/decrypt -d $ENCRYPTED_VALUE -s)
 assertEqual "$TEST_VALUE" "$DECRYPTED_VALUE"
 
 assertCurl 200 "curl $AUTH -k http://$HOST:$PORT/auth/user/info -s"
-assertEqual "user" $(echo $RESPONSE | jq -r .data.username)
-
+assertEqual "user" "$(echo $RESPONSE | jq -r .data.username)"
 
 # Verify access to Swagger and OpenAPI URLs
 #echo "Swagger/OpenAPI tests"
@@ -196,16 +184,14 @@ assertEqual "user" $(echo $RESPONSE | jq -r .data.username)
 #assertEqual "http://$HOST:$PORT" "$(echo $RESPONSE | jq -r '.servers[0].url')"
 #assertCurl 200 "curl -ks  http://$HOST:$PORT/openapi/v3/api-docs.yaml"
 
-if [[ $SKIP_CB_TESTS == "false" ]]
-then
+if [[ $SKIP_CB_TESTS == "false" ]]; then
     testCircuitBreaker
 fi
 
-if [[ $@ == *"stop"* ]]
-then
+if [[ $* == *"stop"* ]]; then
     echo "We are done, stopping the test environment..."
     echo "$ docker compose down"
     docker compose down
 fi
 
-echo "End, all tests OK:" `date`
+echo "End, all tests OK:" $(date)
