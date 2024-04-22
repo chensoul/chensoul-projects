536cdda2fd289a2159812d61b36ead05a49ab3ca
chensoul <ichensoul@gmail.com>
2024-04-22 13:19:51 +0800

    refactor

--- chensoul-audit/chensoul-audit-spring-boot-starter/src/test/resources/GroovyAudit.groovy
+++
@@ -1,9 +0,0 @@
-${logger.info("Groovy audit manager running...")}
-${
-    def bean = applicationContext.getBean("groovyAuditManagerRegistryConfigurer")
-    logger.debug(bean.class.name)
-}
-username: ${username},
-action: ${action},
-operateTime: ${operate_time},
-ip: ${client_ip}
--- chensoul-spring-boot/chensoul-websocket-spring-boot-starter/src/main/java/com/chensoul/spring/boot/websocket/properties/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.chensoul.spring.boot.websocket.properties;
